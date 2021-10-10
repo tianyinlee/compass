b06bfe8d2bc23719e8303f51b1bcf19153dcf18c
Tianyin Lee <5248870+tianyinlee@users.noreply.github.com>
2021-10-10 22:56:58 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 basic.forever(function () {
     if (input.compassHeading() == 90) {
         basic.showArrow(ArrowNames.East)
+    } else if (input.compassHeading() == 180) {
+        basic.showArrow(ArrowNames.South)
     } else {
         basic.showIcon(IconNames.No)
     }
